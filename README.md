dfb533f18ee5cd966590c040ac31484cb1131d7a
Anvar Alimov <alimoffanvar@gmail.com>
2022-11-06 00:38:44 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Tecno Devs Team MCHJ
+
+<i>Manba Kodidan Foydalanish Uchun Qo'llanma</i>
+`ReactJS` yordamida yaratilgan
+
+Foydalanish:
+
+```
+git clone https://github.com/anvaralimov/react-techno
+cd react-techno
+npm start
+```
+
+`http://127.0.0.1:3000`
+
+
+<a href='https://technodevs.team' title="Web Site">technodevs.team</a>
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>TechnoDevsTeam</code> Developers Group 2022
+        </p>
+        <a
+          className="App-link"
+          href="https://t.me/Anvar_Alimov"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Telegram Orqali Bog'lanish
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
